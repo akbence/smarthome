e86780f2476613f12569633de2b85099a1fdd342
Ágocsi-Kiss Bence <bence.agocsi-kiss@icellmobilsoft.hu>
2019-11-22 13:05:41 +0100

    working mqtt subscriber

--- src/main/java/mqtt/Subscriber.java
+++ src/main/java/mqtt/Subscriber.java
@@ -10,47 +10,90 @@ import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.Callable;
 
+public class Subscriber implements Callable<Void> {
 
-public class Subscriber  implements Callable<Void> {
+    public static String DEFAULT_TOPIC = "test";
+    public static String DEFAULT_SERVER_URI = "tcp://broker.hivemq.com";
 
-    public static final String TOPIC = "engine/temperature";
+    private String server;
+    private String topic;
 
     private IMqttClient client;
     private Random rnd = new Random();
 
-    public Subscriber() throws MqttException {
+    public static class SubscriberBuilder {
+
+        private String builderTopic;
+        private String builderServer;
+
+        public SubscriberBuilder withTopic(String topic) {
+            builderTopic = topic;
+            return this;
+        }
+
+        public SubscriberBuilder withServer(String server) {
+            builderServer = server;
+            return this;
+        }
+
+        public Subscriber build() throws MqttException {
+            if (builderTopic == null) {
+                builderTopic = DEFAULT_TOPIC;
+            }
+            if (builderServer == null) {
+                builderServer = DEFAULT_SERVER_URI;
+            }
+            return new Subscriber(this);
+        }
+
+    }
+
+    private Subscriber(SubscriberBuilder builder) throws MqttException {
+        topic = builder.builderTopic;
+        server = builder.builderServer;
         String publisherId = UUID.randomUUID().toString();
-        IMqttClient client = new MqttClient("tcp://iot.eclipse.org:1883",publisherId);
+        client = new MqttClient(server, publisherId);
         this.client = client;
         MqttConnectOptions options = new MqttConnectOptions();
         options.setAutomaticReconnect(true);
         options.setCleanSession(true);
         options.setConnectionTimeout(10);
         client.connect(options);
+
     }
 
+    public void subscribe() throws MqttException {
+        client.subscribe(topic, (top, msg) -> {
+            String payload = new String(msg.getPayload());
+            System.out.println("[I82] Message received: topic={}, payload={}" + payload);
+        });
+    }
+
+    public void unsubscribe() throws MqttException {
+        client.unsubscribe(topic);
+    }
 
     public Void call() throws Exception {
 
-        if ( !client.isConnected()) {
+        if (!client.isConnected()) {
             return null;
         }
 
-        MqttMessage msg = readEngineTemp();
+        MqttMessage msg = new MqttMessage(String.format("pélód").getBytes());
         msg.setQos(0);
         msg.setRetained(true);
-       // client.publish(TOPIC,msg);
 
         return null;
     }
 
     /**
      * This method simulates reading the engine temperature
+     * 
      * @return
      */
     private MqttMessage readEngineTemp() {
-        double temp =  80 + rnd.nextDouble() * 20.0;
-        byte[] payload = String.format("T:%04.2f",temp).getBytes();
+        double temp = 80 + rnd.nextDouble() * 20.0;
+        byte[] payload = String.format("T:%04.2f", temp).getBytes();
         MqttMessage msg = new MqttMessage(payload);
         return msg;
     }
